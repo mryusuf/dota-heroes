8a2d3f086f7cf8e3e0d5cd613010c3265c108f38
mryusuf <muhamadyusufindra@gmail.com>
2021-06-28 20:25:42 +0700

    Initial Commit

---
+++ Dota Heroes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dota Heroes
+//
+//  Created by Indra Permana on 28/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
